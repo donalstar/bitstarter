b21d0ddbc826811abb837bafb748945cdcb45c04
Ubuntu <ubuntu@ip-172-31-21-226.us-west-2.compute.internal>
2014-06-26 21:55:15 +0000

    Changed index

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
+
+var buffer = new Buffer(2000);
+
+var fs = require('fs');
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+
+  buffer.write(data.toString(), "utf-8")
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send(buffer.toString());
 })
 
 app.listen(app.get('port'), function() {
