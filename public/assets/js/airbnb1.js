ae62169008323adc5b37f96b95e346859abfdc77
Donal Carroll <donalstar@gmail.com>
2014-07-05 17:45:35 -0700

    Bootstrap stuff

--- public/assets/js/airbnb1.js
+++ public/assets/js/airbnb1.js
@@ -2,6 +2,9 @@
 
 console.log("Home page helper");
 
+this.initCalendars();
+
+
 AIR.HomepageHelper={curr:0,intervalId:0,numSlides:0,animating:false,time_slideInterval:7000,time_slideTransition:800,time_captionFadeIn:1000,time_captionFadeOut:300,time_searchBarSlide:900,initHomepageHero:function(){this.initEvents();
 this.cacheEls();
 this.showSearch();
